61ab59fabd2deb7d1e2f6b57687a364984ed65e8
Guilherme Goldner da Silva <guilhermegoldnerdasilva@gmail.com>
2019-12-04 15:46:20 -0200

    main.cpp draws a cube that is rotating just for tests. This will be used for the third person camera feature.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include "settings.h"
 #include "xmlutils.h"
 #include "simplesvg.h"
+#include "stopwatch.h"
 
 using namespace std;
 
@@ -36,11 +37,49 @@ public:
 
     static void init() {
         glClearColor(0, 0, 0, 0);
+        vertices[0][0] = vertices[1][0] = vertices[2][0] = vertices[3][0] = -1;
+        vertices[4][0] = vertices[5][0] = vertices[6][0] = vertices[7][0] = 1;
+        vertices[0][1] = vertices[1][1] = vertices[4][1] = vertices[5][1] = -1;
+        vertices[2][1] = vertices[3][1] = vertices[6][1] = vertices[7][1] = 1;
+        vertices[0][2] = vertices[3][2] = vertices[4][2] = vertices[7][2] = 1;
+        vertices[1][2] = vertices[2][2] = vertices[5][2] = vertices[6][2] = -1;
+    }
+
+    // Extracted from:
+    // https://www.opengl.org/archives/resources/code/samples/glut_examples/examples/cube.c
+    // Will delete after the third-person-camera is done
+    static GLfloat light_diffuse[];
+    static GLfloat light_position[];
+    static GLfloat normals[6][3];
+    static GLint faces[6][4];
+    static GLfloat vertices[8][3]; /* Will be filled in with X,Y,Z vertexes. */
+
+    static void drawBox(void) {
+        int i;
+
+        for (i = 0; i < 6; i++) {
+            glBegin(GL_QUADS);
+            glNormal3fv(&normals[i][0]);
+            glVertex3fv(&vertices[faces[i][0]][0]);
+            glVertex3fv(&vertices[faces[i][1]][0]);
+            glVertex3fv(&vertices[faces[i][2]][0]);
+            glVertex3fv(&vertices[faces[i][3]][0]);
+            glEnd();
+        }
     }
 
     static void display() {
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
+        glLoadIdentity();
+        glOrtho(-1, 1, -1, 1, 1, -1);
+
+        // Draw a cube for reference to the camera
+        glColor3f(1, 0, 0);
+        glScaled(0.5f, 0.5f, 0.5f);
+        glRotated(180 * stopwatch_t::currentTimeMillis() / 1000.0f, 0, 1, 1);
+        drawBox();
+
         glutSwapBuffers();
     }
 
@@ -54,8 +93,31 @@ public:
     }
 
     static void idle() {
+        glutPostRedisplay();
     }
 };
+
+
+GLfloat Main::light_diffuse[] = {1.0, 0.0, 0.0, 1.0}; /* Red diffuse light. */
+GLfloat Main::light_position[] = {1.0, 1.0, 1.0, 0.0}; /* Infinite light location. */
+GLfloat Main::normals[6][3] = {/* Normals for the 6 faces of a cube. */
+    {-1.0, 0.0, 0.0},
+    {0.0, 1.0, 0.0},
+    {1.0, 0.0, 0.0},
+    {0.0, -1.0, 0.0},
+    {0.0, 0.0, 1.0},
+    {0.0, 0.0, -1.0}
+};
+GLint Main::faces[6][4] = {/* Vertex indices for the 6 faces of a cube. */
+    {0, 1, 2, 3},
+    {3, 2, 6, 7},
+    {7, 6, 5, 4},
+    {4, 5, 1, 0},
+    {5, 6, 2, 1},
+    {7, 4, 0, 3}
+};
+GLfloat Main::vertices[8][3]; /* Will be filled in with X,Y,Z vertexes. */
+
 
 // As stated in spec, the size of the window is initially 500x500.
 int Main::sWidth = 500;
