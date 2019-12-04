27457a7c4d3d9f9494e0d256b96e034b1c4d1148
Guilherme Goldner da Silva <guilhermegoldnerdasilva@gmail.com>
2019-12-04 15:17:49 -0200

    GLUT being initialized and a window is opening. A bug fix with the settings loading.

--- main.cpp
+++ main.cpp
@@ -17,35 +17,96 @@ using namespace std;
 
 #define TF_VERSION "0.1.2"
 
-void loadSettings(char* configFile, app_settings& settings) {
+app_settings* loadSettings(char* configFile) {
     file_path path(configFile);
 
     path.append("config.xml");
 
     printf("Trying to open %s\n", path.toString());
 
-    settings = app_settings(path.toString());
+    return new app_settings(path.toString());
 }
+
+/**
+ * A static class, just to not use global variables, as asked in a previous TC.
+ */
+class Main {
+public:
+
+    static int sWidth;
+    static int sHeight;
+
+    static void init() {
+        glClearColor(0, 0, 0, 0);
+    }
+
+    static void display() {
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glutSwapBuffers();
+    }
+
+    static void reshape(int width, int height) {
+        sWidth = width;
+        sHeight = height;
+
+        glViewport(0, 0, width, height);
+
+        printf("Reshape: (%d, %d)\n", width, height);
+    }
+
+    static void idle() {
+    }
+};
+
+// As stated in spec, the size of the window is initially 500x500.
+int Main::sWidth = 500;
+int Main::sHeight = 500;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
+    printf("myself: %s\n", argv[0]);
 
-    app_settings settings;
+    glutInit(&argc, argv);
 
     try {
         if (argc < 2) {
             printf("syntax: %s <config folder>\n", argv[0]);
             return EXIT_SUCCESS;
         }
-        
-        loadSettings(argv[1], settings);
+
+        app_settings* settings = loadSettings(argv[1]);
+
+        Main::init();
 
         // TODO create game context (objects, GLUT, etc)
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+        std::string title("TC4 - Guilherme, Ricardo, ");
+        title += TF_VERSION;
+
+        glutCreateWindow(title.c_str());
+
+        glutReshapeFunc(Main::reshape);
+        glutDisplayFunc(Main::display);
+
+        // glutKeyboardFunc(keyDown);
+        // glutKeyboardUpFunc(keyUp);
+
+        // glutMouseFunc(mouseButton);
+        // glutPassiveMotionFunc(passiveMotion);
+
+        glutIdleFunc(Main::idle);
+
+        glutInitWindowSize(Main::sWidth, Main::sHeight);
+        glutReshapeWindow(Main::sWidth, Main::sHeight);
+        // glutInitWindowPosition(100, 20);
 
         printf("Game context created\n");
+
+        glutMainLoop();
     } catch (IOException& ex) {
         printf("[error] An exception: %s\n", ex.what());
     } catch (MissingElementException& ex) {
