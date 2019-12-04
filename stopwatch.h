4f14243d336a57a0ddb2f2ce9a9dac9ef5e8d7e1
Guilherme Goldner da Silva <guilhermegoldnerdasilva@gmail.com>
2019-12-04 15:19:23 -0200

    virtual removed from destructor.

--- stopwatch.h
+++ stopwatch.h
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   stopwatch.h
  * Author: gg
@@ -35,7 +29,7 @@ public:
         reset();
     }
 
-    virtual ~stopwatch();
+    ~stopwatch();
 
     /**
      * Restarts this stopwatch at the current time.
