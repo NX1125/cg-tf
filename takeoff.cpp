71ea3d9b7ba5c6c6567bd0747b868c2256afccaf
Guilherme Goldner da Silva <guilhermegoldnerdasilva@gmail.com>
2019-12-04 22:30:47 -0300

    takeoff files created, but not implemented.

---
+++ takeoff.cpp
@@ -0,0 +1,8 @@
+/* 
+ * File:   takeoff_t.cpp
+ * Author: gg
+ * 
+ * Created on 4 de dezembro de 2019, 23:27
+ */
+
+#include "takeoff.h"
---
+++ takeoff.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   takeoff_t.h
+ * Author: gg
+ *
+ * Created on 4 de dezembro de 2019, 23:27
+ */
+
+#ifndef TAKEOFF_T_H
+#define TAKEOFF_T_H
+
+class takeoff_t {
+};
+
+#endif /* TAKEOFF_T_H */
+
