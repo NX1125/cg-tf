c193bfef79f5e36e4dd1e9154df1930aee0736a3
Guilherme Goldner da Silva <guilhermegoldnerdasilva@gmail.com>
2019-12-04 23:32:27 -0200

    Movement classes created, but not implemented.

---
+++ airplanemovement.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   airplane_movement_t.h
+ * Author: gg
+ *
+ * Created on 4 de dezembro de 2019, 23:31
+ */
+
+#ifndef AIRPLANE_MOVEMENT_T_H
+#define AIRPLANE_MOVEMENT_T_H
+
+class airplane_movement_t {
+};
+
+#endif /* AIRPLANE_MOVEMENT_T_H */
+
