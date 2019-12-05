2d1015232d0526d5f49d3b7aadff7f8f62d847aa
Guilherme Goldner da Silva <guilhermegoldnerdasilva@gmail.com>
2019-12-05 00:02:27 -0200

    Airplane movement class has listener for the mouse movement.

--- airplanemovement.h
+++ airplanemovement.h
@@ -9,6 +9,89 @@
 #define AIRPLANE_MOVEMENT_T_H
 
 class airplane_movement_t {
+private:
+
+    // cached keys
+
+    float left;
+    float right;
+    float up;
+    float down;
+
+    float velocity;
+    float velocityAccelerationFactor = 0.05f;
+
+    vector3f velocityAxis;
+
+    point3f previous;
+    point3f position;
+
+public:
+
+    void keyPress(unsigned char key) {
+        updateKey(key, 1);
+    }
+
+    void keyReleased(unsigned char key) {
+        updateKey(key, 0);
+    }
+
+private:
+
+    void updateKey(unsigned char key, float intensity) {
+        switch (key) {
+            case 'a':
+                left = intensity;
+                break;
+            case 'd':
+                right = intensity;
+                break;
+            case 's':
+                down = intensity;
+                break;
+            case 'w':
+                up = intensity;
+                break;
+            case '+':
+                velocity *= 1 + velocityAccelerationFactor;
+                return;
+            case '-':
+                velocity *= 1 - velocityAccelerationFactor;
+                return;
+            default:
+                return;
+        }
+
+        updateAxis();
+    }
+
+public:
+
+    void move(float dt) {
+        previous = position;
+        position = previous + velocityAxis * (dt * velocity);
+    }
+
+    float getVerticalAngle() const {
+        return up - down;
+    }
+
+    float getHorizontalAngle() const {
+        return right - left;
+    }
+
+private:
+
+    void updateAxis() {
+        float v = getVerticalAngle();
+        float h = getVerticalAngle();
+
+        float g = cosf(v);
+
+        velocityAxis.x = g * cosf(h);
+        velocityAxis.y = g * sinf(h);
+        velocityAxis.z = sinf(v);
+    }
 };
 
 #endif /* AIRPLANE_MOVEMENT_T_H */
